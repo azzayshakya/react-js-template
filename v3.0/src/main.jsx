e5665669623158cb0944908d8dc95cfb90973480
Ajay <ajayshakya7376@gmail.com>
2026-06-18 07:15:29 +0530

    new Version release

---
+++ v3.0/src/main.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import { QueryClientProvider } from '@tanstack/react-query'
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+// import { queryClient } from 'api/queryClient'
+// import { AuthProvider } from 'context/AuthContext'
+// import { ThemeProvider } from 'context/ThemeContext'
+// import { ToastProvider } from 'context/ToastContext'
+import { AppRouter } from 'router'
+import 'styles/tokens.css'
+import 'styles/globals.css'
+import VersionUpdate from 'components/ui/VersionUpdate'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <QueryClientProvider client={queryClient}> */}
+    {/* <AuthProvider> */}
+    {/* <ThemeProvider> */}
+    {/* <ToastProvider> */}
+    {/* <AppRouter /> */}
+    <VersionUpdate />
+    {/* </ToastProvider> */}
+    {/* </ThemeProvider> */}
+    {/* </AuthProvider> */}
+    {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+    {/* </QueryClientProvider> */}
+  </React.StrictMode>
+)
