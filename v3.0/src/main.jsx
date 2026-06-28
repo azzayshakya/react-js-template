7272faea26887ead9f79a73bc3a3fcf0b7ff9480
Ajay <ajayshakya7376@gmail.com>
2026-06-28 12:24:57 +0530

    THEME INTEGRAION ALONG Whole Project .

--- v3.0/src/main.jsx
+++ v3.0/src/main.jsx
@@ -1,33 +1,15 @@
+import ThemeProvider from '@devStack/constants/Theme-Provider'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { AppRouter } from './router'
-import './index.css'
-// import { QueryClientProvider } from '@tanstack/react-query'
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-// import { queryClient } from 'api/queryClient'
-// import { AuthProvider } from 'context/AuthContext'
-// import { ThemeProvider } from 'context/ThemeContext'
-// import { ToastProvider } from 'context/ToastContext'
-// import { AppRouter } from 'router'
-// import 'styles/tokens.css'
-// import 'styles/globals.css'
 
-// const { isPending } = useValidateUserSession()
-//   useAuthSync()
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <QueryClientProvider client={queryClient}> */}
-    {/* <AuthProvider> */}
-    {/* <ThemeProvider> */}
-    {/* <ToastProvider> */}
-    <AppRouter />
-    {/* <VersionUpdate /> */}
-    {/* </ToastProvider> */}
-    {/* </ThemeProvider> */}
-    {/* </AuthProvider> */}
-    {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-    {/* </QueryClientProvider> */}
+    <ThemeProvider>
+      <AppRouter />
+    </ThemeProvider>
   </React.StrictMode>
 )
